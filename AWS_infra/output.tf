285c29a2462b72497f421aed76274c4494d2d7b1
David <david@ISMAD0811.innotec.inet>
2022-07-26 18:07:45 +0200

    Test for integrate ansible role

--- AWS_infra/output.tf
+++ AWS_infra/output.tf
@@ -10,10 +10,22 @@ output "caldera_server_ip" {
   value = aws_instance.caldera_server.public_ip
 }
 
+output "caldera_server_id" {
+  value = aws_instance.caldera_server.id
+}
+
 output "linux_host_ip" {
   value = aws_instance.linux_host.private_ip
 }
 
+output "linux_host_id" {
+  value = aws_instance.linux_host.id
+}
+
 output "windows_host_ip" {
   value = aws_instance.windows_host.private_ip
+}
+
+output "windows_host_id" {
+  value = aws_instance.windows_host.id
 }
