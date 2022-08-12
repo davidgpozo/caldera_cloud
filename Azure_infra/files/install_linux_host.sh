00dcf21e644d9a1881dc8e7c5d5bff641fe56d05
David Gonzalez <davidgpozo@gmail.com>
2022-08-12 14:30:13 +0200

    Delete Azure_infra directory

--- Azure_infra/files/install_linux_host.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#while ! curl -H "KEY:redAPIKeySantander22" -X DELETE http://172.23.4.95:2288/api/rest -d '{"index":"agents","paw":"$agent_paw"}' &>/dev/null; do echo "Curl Fail - `date`" && sleep 1; done
-server="http://172.23.4.95:2288";
-agent=$(curl -svkOJ -X POST -H "file:sandcat.go" -H "platform:linux" $server/file/download 2>&1 | grep -i "Content-Disposition" | grep -io "filename=.*" | cut -d'=' -f2 | tr -d '"\r') && chmod +x $agent 2>/dev/null;
-nohup ./$agent -server $server &
-
--- Azure_infra/terraform_modules/azure_rsa_key/output.tf
+++
@@ -1,7 +0,0 @@
-output "private_rsa_key" {
-  value = tls_private_key.ssh_key.private_key_pem
-}
-
-output "ssh_public_name" {
-  value = azurerm_ssh_public_key.worker.name
-}
