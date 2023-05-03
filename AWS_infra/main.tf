7731c1aa84f03657543de3f246ff81e5e8c6ab72
David Gonzalez <davidgpozo@gmail.com>
2023-05-03 15:53:19 +0200

    Update main.tf

--- AWS_infra/main.tf
+++ AWS_infra/main.tf
@@ -54,9 +54,9 @@ resource "aws_security_group" "sg_caldera_server" {
 
   ingress {
     description = "Caldera server port"
-    from_port   = 2288
-    to_port     = 2288
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
